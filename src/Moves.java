005c0b478d92eb89a7e143b73a3d07546f6d52b1
Yoshio <yoshiok1119@gmail.com>
2020-12-31 00:10:42 -0500

    New implementation of finding moves. Should be faster

--- src/Moves.java
+++ src/Moves.java
@@ -9,7 +9,7 @@ public class Moves {
     // Creating constants for parts of the board for easy checking down the line.
     static long RANK1 = -72057594037927936L; //"1111111100000000000000000000000000000000000000000000000000000000"
     static long RANK8 = 255L; //"0000000000000000000000000000000000000000000000000000000011111111"
-    static long RANK4 = 1095216660480L; //"0000000000000000000000001111111100000000000000000000000000000000"
+        static long RANK4 = 1095216660480L; //"0000000000000000000000001111111100000000000000000000000000000000"
     static long RANK5 = 4278190080L; //"0000000000000000000000000000000011111111000000000000000000000000"
     static long FILEA = 72340172838076673L; //"0000000100000001000000010000000100000001000000010000000100000001"
     static long FILEH = -9187201950435737472L; //"1000000010000000100000001000000010000000100000001000000010000000"
@@ -29,50 +29,72 @@ public class Moves {
 
         // Note: Moves are in this format: startingx startingy endingx endingy (no spaces, commas, etc.)
 
-        String allWhiteMoves = possibleWPMoves(WP) + possibleWNMoves(WN);
+        String history = ""; // History is empty for now.
+
+        String allWhiteMoves = possibleWPMoves(WP, BP, history) + possibleWNMoves(WN);
 
         return allWhiteMoves;
 
     }
 
-    public static String possibleWPMoves(long WP){
+
+
+    public static String possibleWPMoves(long WP, long BP, String history){
         String allWPMoves = "";
 
         // All legal moves for a white pawn capturing to the right.
         long PAWN_RIGHT = (WP>>7)&(BLACK_PIECES)&~(RANK8)&~(FILEA);
 
-        // For every square between the first legal move to the last legal move record the move. Excluding leading 0s
-        // and trailing 0s reduces the number of squares to check without missing possible moves. (Especially for when
-        // there is only one piece of a certain type)
+        // Obtaining the bitboard containing only the first move. Note: (PAWN_RIGHT-1) is the bitboard with only the
+        // first move as 0 while the rest of the moves are 1.
+        // So ~(PAWN_RIGHT-1) is the bitboard with only the first move as 1 with the rest of the moves as 0.
+        // Then PAWN_RIGHT&(~PAWN_RIGHT-1) would be only the first move.
 
-        for (int i = Long.numberOfLeadingZeros(PAWN_RIGHT); i < 64-Long.numberOfTrailingZeros(PAWN_RIGHT); i++){
-            if (((PAWN_RIGHT>>i)&1) == 1){
-                // i/8 and i %8 represents the ending coordinates so we must change the starting coordinate accordingly.
-                allWPMoves += (i/8 + 1) + (i%8 - 1) + (i/8) + (i % 8);
-            }
+        // EXAMPLE: m = 0010100      m - 1 = 0010011     ~(m-1)= 1101100     m&~(m-1) = 000100 only the first move is 1.
+
+        long aMove = PAWN_RIGHT&(~PAWN_RIGHT-1);
+
+        while(aMove != 0){
+
+            // Getting the square of the move.
+            int i = Long.numberOfTrailingZeros(aMove);
+            // Recording the move with the starting and ending coordinates.
+            allWPMoves += (i/8 + 1) + (i%8 - 1) + (i/8) + (i%8);
+
+            // Removing the move from the total move bitboard.
+            PAWN_RIGHT = PAWN_RIGHT&~aMove;
+
+            // Doing the same thing as before but we move onto the next move.
+            aMove = PAWN_RIGHT&(~PAWN_RIGHT-1);
         }
         // All legal moves for a white pawn capturing to the left.
         long PAWN_LEFT = (WP>>9)&(BLACK_PIECES)&~(RANK8)&~(FILEH);
-        for (int i = Long.numberOfLeadingZeros(PAWN_LEFT); i < 64-Long.numberOfTrailingZeros(PAWN_LEFT); i++){
-            if (((PAWN_RIGHT>>i)&1) == 1){
-                allWPMoves += (i/8 + 1) + (i%8 + 1) + (i/8) + (i % 8);
-            }
+        aMove = PAWN_LEFT&(~PAWN_LEFT-1);
+        while(aMove != 0){
+            int i = Long.numberOfTrailingZeros(aMove);
+            allWPMoves += (i/8 + 1) + (i%8 + 1) + (i/8) + (i % 8);
+            PAWN_LEFT = PAWN_LEFT&~aMove;
+            aMove = PAWN_LEFT&(~PAWN_LEFT-1);
         }
-
         // All legal moves for pushing a white pawn 1 square.
         long PAWN_PUSH = (WP>>8)&(EMPTY)&~(RANK8);
-        for (int i = Long.numberOfLeadingZeros(PAWN_PUSH); i < 64-Long.numberOfTrailingZeros(PAWN_PUSH); i++){
-            if (((PAWN_RIGHT>>i)&1) == 1){
-                allWPMoves += (i/8 + 1) + (i%8) + (i/8) + (i % 8);
-            }
-        }
+        aMove = PAWN_PUSH&(~PAWN_PUSH-1);
 
+        while(aMove != 0){
+            int i = Long.numberOfTrailingZeros(aMove);
+            allWPMoves += (i/8 + 1) + (i%8) + (i/8) + (i % 8);
+            PAWN_PUSH = PAWN_PUSH&~aMove;
+            aMove = PAWN_PUSH&(~PAWN_PUSH-1);
+        }
         // All legal moves for pushing a white pawn 2 squares. (this can only happen when the pawn is on the 2nd rank)
         long PAWN_LEAP = (WP>>16)&(EMPTY)&(EMPTY>>8)&(RANK4);
-        for (int i = Long.numberOfLeadingZeros(PAWN_LEAP); i < 64-Long.numberOfTrailingZeros(PAWN_LEAP); i++){
-            if (((PAWN_RIGHT>>i)&1) == 1){
-                allWPMoves += (i/8 + 2) + (i%8) + (i/8) + (i % 8);
-            }
+        aMove = PAWN_LEAP&(~PAWN_LEAP-1);
+
+        while(aMove != 0){
+            int i = Long.numberOfTrailingZeros(aMove);
+            allWPMoves += (i/8 + 2) + (i%8) + (i/8) + (i % 8);
+            PAWN_LEAP = PAWN_LEAP&~aMove;
+            aMove = PAWN_LEAP&(~PAWN_LEAP-1);
         }
 
         // Promotions:
@@ -80,25 +102,46 @@ public class Moves {
 
         // All legal moves for pushing a white pawn 1 square with promotion.
         long PAWN_PROMOTION_FORWARD = (WP>>8)&(RANK8)&(EMPTY);
-        for (int i = Long.numberOfLeadingZeros(PAWN_PROMOTION_FORWARD); i < 64-Long.numberOfTrailingZeros(PAWN_PROMOTION_FORWARD); i++){
-            if (((PAWN_RIGHT>>i)&1) == 1){
-                allWPMoves += (i/8 + 1) + (i%8) + (i/8) + (i % 8) + "P";
-            }
+        aMove = PAWN_PROMOTION_FORWARD&(~PAWN_PROMOTION_FORWARD-1);
+
+        while(aMove != 0) {
+            int i = Long.numberOfTrailingZeros(aMove);
+            allWPMoves += (i / 8 + 1) + (i % 8) + (i / 8) + (i % 8) + "P";
+            PAWN_PROMOTION_FORWARD = PAWN_PROMOTION_FORWARD & ~aMove;
+            aMove = PAWN_PROMOTION_FORWARD & (~PAWN_PROMOTION_FORWARD - 1);
         }
         // All legal moves for capturing to the right with promotion.
         long PAWN_PROMOTION_RIGHT = (WP>>7)&(RANK8)&(BLACK_PIECES)&~(FILEA);
-        for (int i = Long.numberOfLeadingZeros(PAWN_PROMOTION_RIGHT); i < 64-Long.numberOfTrailingZeros(PAWN_PROMOTION_RIGHT); i++){
-            if (((PAWN_RIGHT>>i)&1) == 1){
-                allWPMoves += (i/8 + 1) + (i%8 - 1) + (i/8) + (i % 8) + "P";
-            }
+        aMove = PAWN_PROMOTION_RIGHT&(~PAWN_PROMOTION_RIGHT-1);
+
+        while(aMove != 0) {
+            int i = Long.numberOfTrailingZeros(aMove);
+            allWPMoves += (i/8 + 1) + (i%8 - 1) + (i/8) + (i % 8) + "P";
+            PAWN_PROMOTION_RIGHT = PAWN_PROMOTION_RIGHT & ~aMove;
+            aMove = PAWN_PROMOTION_RIGHT & (~PAWN_PROMOTION_RIGHT - 1);
         }
         // All legal moves for capturing to the left with promotion.
         long PAWN_PROMOTION_LEFT = (WP>>9)&(RANK8)&(BLACK_PIECES)&~(FILEH);
-        for (int i = Long.numberOfLeadingZeros(PAWN_PROMOTION_LEFT); i < 64-Long.numberOfTrailingZeros(PAWN_PROMOTION_LEFT); i++){
-            if (((PAWN_RIGHT>>i)&1) == 1){
-                allWPMoves += (i/8 + 1) + (i%8 + 1) + (i/8) + (i % 8) + "P";
-            }
+        aMove = PAWN_PROMOTION_LEFT&(~PAWN_PROMOTION_LEFT-1);
+
+        while(aMove != 0) {
+            int i = Long.numberOfTrailingZeros(aMove);
+            allWPMoves += (i/8 + 1) + (i%8 + 1) + (i/8) + (i % 8) + "P";
+            PAWN_PROMOTION_LEFT = PAWN_PROMOTION_LEFT & ~aMove;
+            aMove = PAWN_PROMOTION_LEFT & (~PAWN_PROMOTION_LEFT - 1);
         }
+
+        // En Passants:
+
+        //if (history.length() >= 16){ // At least 4 plys have been made (There are no positions before 4 plys have been
+                                     // made where an en passant is possible.
+
+
+
+        //}
+
+
+
         return allWPMoves;
 
 
